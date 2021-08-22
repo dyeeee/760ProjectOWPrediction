fcf28223008e0749ab8a75b72d47a20ecba82a6c
Nayoth <zyxblah@qq.com>
2021-08-22 16:54:05 +0800

    初步选手排名

--- Glicko/glicko_player_rating.py
+++ Glicko/glicko_player_rating.py
@@ -1,29 +1,82 @@
 import glicko2
 import pymysql
 import pandas as pd
+import numpy as np
 
-class TeamRating(object):
+class PlayerRating(object):
 
-    def __init__(self, df_result):
+    def __init__(self, df_result, df_heros):
         self._result = df_result
-        self._rate = dict()
+        self._heros = df_heros
+        self._teamrate = dict()
+        self._playerRate = dict()
+        self._matchDict = dict()
+        self._zscore = list()
+
+    def InitTeams(self):
+        for team in self._result['match_winner']:
+            if team not in self._teamrate:
+                self._teamrate[team] = glicko2.Player()
+        for team in self._result['match_loser']:
+            if team not in self._teamrate:
+                self._teamrate[team] = glicko2.Player()
 
     def InitPlayers(self):
-        for team in self._result['match_winner']:
-            if team not in self._rate:
-                self._rate[team] = glicko2.Player()
-        for team in self._result['match_loser']:
-            if team not in self._rate:
-                self._rate[team] = glicko2.Player()
+        for player in self._heros['player_name']:
+            if(player not in self._playerRate):
+                self._playerRate[player] = glicko2.Player()
+
+    def InitmatchDict(self):
+        for data in self._heros.itertuples():
+            matchId = data[1]
+            playerName = data[2]
+            if(matchId not in self._matchDict):
+                self._matchDict[matchId] = dict()
+            if(playerName not in self._matchDict[matchId]):
+                self._matchDict[matchId][playerName] = list([data[3], 0, 0])
+            self._matchDict[matchId][playerName][1] += float(data[4])
+            self._matchDict[matchId][playerName][2] += float(data[5])
+
+    def InitNorm(self):
+        dps = list()
+        for match in self._matchDict.values():
+            for player in match.values():
+                dps.append(player[1]/player[2])
+        self._zscore.append(np.average(dps))
+        self._zscore.append(np.std(dps))
+
+    def DpsNormalization(self, dps):
+        return (dps - self._zscore[0])/self._zscore[1]
+
 
     def CalcualteRate(self):
+        self.InitTeams()
         self.InitPlayers()
+        self.InitmatchDict()
+        self.InitNorm()
         for _, outcome in self._result.iterrows():
-            winner = self._rate[outcome[1]]
-            loser = self._rate[outcome[2]]
+            matchId = outcome[0]
+            winner = self._teamrate[outcome[1]]
+            loser = self._teamrate[outcome[2]]
             loser_rating = loser.rating
             loser_rd = loser.rd
-            loser.update_player([winner.rating], [winner.rd], [0])
+            winner_rating = winner.rating
+            winner_rd = winner.rd
+
+            if(int(matchId) == 34844):
+                return
+
+            for player, inf in self._matchDict[matchId].items():
+                playerRate = self._playerRate[player]
+                normDps =  self.DpsNormalization(inf[1]/inf[2])
+                if(winner == inf[0]):
+                    playerRate.update_player([loser_rating], [loser_rd], [1 + normDps])
+                else:
+                    playerRate.update_player([winner_rating], [winner_rd], [normDps])
+
+
+
+            loser.update_player([winner_rating], [winner_rd], [0])
             winner.update_player([loser_rating], [loser_rd], [1])
 
 def takeSecond(elem):
@@ -43,11 +96,17 @@ if __name__ == '__main__':
 
     df_result = pd.DataFrame(list(result), columns=["match_id", "match_winner", "match_loser"])
 
-    TR = TeamRating(df_result)
+    cur.execute("select esports_match_id, player_name, team_name, stat_amount, time_played from all_heroes_damage_2020_1")
+    hero = cur.fetchall()
+    df_hero = pd.DataFrame(list(hero), columns=["match_id", "player_name", "team_name", "damage_amount", "time_played"])
+
+
+
+    TR = PlayerRating(df_result, df_hero)
     TR.CalcualteRate()
     a = list()
-    for team in sorted (TR._rate):
-        a.append((team, TR._rate[team].rating))
+    for player in sorted (TR._playerRate):
+        a.append((player, TR._playerRate[player].rating))
         #print(str(team) + "\t" + str(TR._rate[team].rating))
     a.sort(key = takeSecond, reverse=True)
     i = 1
